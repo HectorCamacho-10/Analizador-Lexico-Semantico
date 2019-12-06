a04057e26379c7d161d380563c5505de754bcea5
Jesus Geovany Dzul Canul <geovany.jg14@gmail.com>
2019-12-06 15:06:37 -0600

    -

---
+++ texto.js
@@ -0,0 +1,31 @@
+var a = -3;
+var b = 3.1416;
+
+var c = a**b;
+console.log(c);
+
+if(c==10){
+    console.log("El numero es mayor a 10");
+}else{
+    console.log("El numero es menor a 10");
+}
+if(c<=10){
+    console.log("s")
+}
+
+var j = 10;
+var r = 2;
+var re = 10*2;
+var ree =10**2;
+var reee = 10-2;
+console.log(ree)
+var jum = [1,3,4,6,7];
+var name='luis';
+var lastname="mendez medez";
+let ab = 0;
+var jssum = ["a","b","s", "pedro"];
+var mr = true;
+var rj = false;
+var fullname = name+lastname;
+
+console.log(fullname);
